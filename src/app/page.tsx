6f6c14a0cb2e8341b81cf91fd25ba4c40b0358fa
micci184 <micci184@gmail.com>
2024-07-08 15:58:23 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import CompanyInfo from "@/components/CompanyInfo";
+import CompanyNameOrigin from "@/components/CompanyNameOrigin";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import MainSection from "@/components/MainSection";
 import ServiceSection from "@/components/ServiceSection";
-import CompanyNameOrigin from "@/components/CompanyNameOrigin";
 
 export default function Page() {
   return (
