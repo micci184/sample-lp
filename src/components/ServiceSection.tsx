2a3c5b808f431cb6a9b0a759c27b8a7a98602248
micci184 <micci184@gmail.com>
2024-07-09 00:23:14 +0900

    mod header

--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const ServiceSection: React.FC = () => {
   return (
-    <div className="px-4 sm:px-10 md:px-20 lg:px-40 pt-8 sm:pt-16 md:pt-24 lg:pt-32 pb-8 sm:pb-10 md:pb-10 lg:pb-10 min-h-[200px] md:min-h-[300px] lg:min-h-[400px]">
+    <div
+      id="service"
+      className="px-4 sm:px-10 md:px-20 lg:px-40 pt-8 sm:pt-16 md:pt-24 lg:pt-32 pb-8 sm:pb-10 md:pb-10 lg:pb-10 min-h-[200px] md:min-h-[300px] lg:min-h-[400px]"
+    >
       <div className="flex items-end mb-4">
         <h2 className="text-xl sm:text-2xl font-bold text-white bg-darkGray px-4 py-2 w-full md:w-[350px]">
           Service
