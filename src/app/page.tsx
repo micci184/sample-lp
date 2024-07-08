c104600c4a5ab958cf98be0439246e016455e968
micci184 <micci184@gmail.com>
2024-07-08 10:06:02 +0900

    create base components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import React from "react";
+
+import CompanyInfo from "@/components/CompanyInfo";
+import CompanyMission from "@/components/CompanyMission";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import MainSection from "@/components/MainSection";
+
 export default function Page() {
-  return <h1>Page Test</h1>;
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <CompanyMission />
+      <CompanyInfo />
+      <Footer />
+    </div>
+  );
 }
