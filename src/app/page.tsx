52a61c81441fac955088156f116907bd0a94c8a0
micci184 <micci184@gmail.com>
2024-07-08 15:57:12 +0900

    create service and origin name section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 import CompanyInfo from "@/components/CompanyInfo";
-import CompanyMission from "@/components/CompanyMission";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import MainSection from "@/components/MainSection";
+import ServiceSection from "@/components/ServiceSection";
+import CompanyNameOrigin from "@/components/CompanyNameOrigin";
 
 export default function Page() {
   return (
     <div>
       <Header />
       <MainSection />
-      <CompanyMission />
+      <ServiceSection />
+      <CompanyNameOrigin />
       <CompanyInfo />
       <Footer />
     </div>
---
+++ src/components/CompanyNameOrigin.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CompanyNameOrigin: React.FC = () => {
+	return (
+    <div className="p-20 h-[400px]">
+      <div className="flex items-end mb-4">
+        <h2 className="text-2xl font-bold text-white bg-darkGray px-4 py-2 w-[350px]">
+          Origin of company name
+        </h2>
+        <div className="flex-1 border-b-2 border-darkGray"></div>
+      </div>
+      <div>
+        <p className="mb-6 pt-2 text-lg">
+          ログスペクト（Logspect）とは、ログ（Log）とスペクト（Spect）を組み合わせた造語です。
+          Spect
+          は、ラテン語の「spectare」を起源としている英単語で「見る」「観察する」を意味します。
+          Spect 単体では使わず、inspect（検査する）や
+          respect（尊敬）などの単語として利用されています。
+          「ログ」というファクトに基づいた意思決定をするためのログを「見る」ための環境の発展に貢献したい
+          という想い、また「ログ」に対する「敬意」の気持ちを込めています。
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyNameOrigin;
