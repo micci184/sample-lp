e330c5ae4cbd6137be6ac2645bfe91ae42f241fb
micci184 <micci184@gmail.com>
2024-07-10 08:16:53 +0900

    mod contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact: React.FC = () => {
   return (
     <section className="px-4 sm:px-6 md:px-10 lg:px-16 py-20 sm:py-25 md:py-25 lg:py-25 text-center bg-gray-100">
       <p className="text-sm sm:text-xs mb-4 japanese-font">
-        弊社へのお問い合わせはこちらよりお願いいたします。
+        当社へのお問い合わせはこちらよりお願いいたします。
       </p>
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLSdY_ATIkPvriNeXrj6y7ZIg6A_7b-tXvZ1Gyl-40ceoSRHpSQ/viewform"
